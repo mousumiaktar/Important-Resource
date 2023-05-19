70a618c2554e6ddd8dcad7410c865da8987f30f6
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-05-19 18:17:08 +0600

    importance resources

---
+++ GitClone/ReactQuery/Query.js
@@ -0,0 +1,35 @@
+const { data: places, isLoading, refetch } = useQuery('places', () => fetch('https://comming-italy.onrender.com/places', {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(res => res.json()))
+
+    if (isLoading) {
+        return <Loading></Loading>
+
+    }
+
+
+
+    // fetch
+    {places.map((place =>
+
+        <Place key={place._id} place={place} refetch={refetch}></Place>
+    ))}
+
+
+
+//edit index js file simple 
+//go to react query web site see details
+
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+ const queryClient = new QueryClient()
+
+ 
+  
+    //  <QueryClientProvider client={queryClient}>
+    //    <Example />
+    //  </QueryClientProvider>
+   
+ 
